9465dacb390606ef421d58a79b51499c1db9aa8e
Kuroko <982239116@qq.com>
2023-05-06 17:40:30 +0800

    接口测试完善，使用idea自带的httpclient测试接口

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -4,12 +4,14 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Arrays;
+
+import javax.annotation.Resource;
 
 /**
  * @author Kuroko
@@ -19,17 +21,12 @@ import java.util.Arrays;
 @RestController
 @Api(value = "课程信息编辑接口", tags = "课程信息编辑接口")
 public class CourseBaseInfoController {
+    @Resource
+    CourseBaseInfoService courseBaseInfoService;
     @PostMapping("/course/list")
     @ApiOperation("课程查询接口")
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody QueryCourseParamsDto queryCourseParams) {
-        CourseBase courseBase = new CourseBase();
-        courseBase.setId(15L);
-        courseBase.setDescription("测试课程");
-        PageResult<CourseBase> result = new PageResult<>();
-        result.setItems(Arrays.asList(courseBase));
-        result.setPage(1);
-        result.setPageSize(10);
-        result.setCounts(1);
+        PageResult<CourseBase> result = courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParams);
         return result;
     }
 }
