4a3459b3da69a559dc5b7e33be7b61b4eb03b95d
Kuroko <982239116@qq.com>
2023-05-08 15:34:53 +0800

    测试接口

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
@@ -1,12 +1,14 @@
 package com.xuecheng.content.api;
 
 import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
+import com.xuecheng.content.service.CourseCatogoryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -18,6 +20,8 @@ import java.util.List;
 @RestController
 @Api(value = "课程分类相关接口", tags = "课程分类相关接口")
 public class CourseCategoryController {
+    @Resource
+    CourseCatogoryService courseCatogoryService;
     /**
      * 返回课程分类树形结构
      * @return
@@ -25,6 +29,6 @@ public class CourseCategoryController {
     @GetMapping("/course-category/tree-nodes")
     @ApiOperation("课程分类相关接口")
     public List<CourseCategoryTreeDto> queryTreeNodes(){
-        return null;
+        return courseCatogoryService.queryTreeNodes("1");
     }
 }
