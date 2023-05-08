61769f09e56ac3c272bd30fb31a103fdc54eadf0
Kuroko <982239116@qq.com>
2023-05-08 20:11:24 +0800

    JSR303校验。对参数的基本合法性进行校验。 前端请求后端接口传输参数时，需要在Controller和Service中校验。Controller中校验请求参数的合法性，Service中校验业务相关内容。

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/GlobalExceptionHandler.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/GlobalExceptionHandler.java
@@ -1,11 +1,18 @@
 package com.xuecheng.base.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Kuroko
@@ -30,4 +37,22 @@ public class GlobalExceptionHandler {
         log.error("系统异常:{}", e.getMessage(), e);
         return new RestErrorResponse(CommonError.UNKOWN_ERROR.getErrMessage());
     }
+
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public RestErrorResponse doMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        // 由于用户输入的内容可能存在多处错误，所以我们要将所有错误信息都提示给用户
+        BindingResult bindingResult = exception.getBindingResult();
+        // 获取错误集合
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        // 拼接字符串
+        StringBuffer stringBuffer = new StringBuffer();
+        fieldErrors.forEach(fieldError -> stringBuffer.append(fieldError.getDefaultMessage()).append(","));
+        // 记录日志
+        log.error(stringBuffer.toString());
+        // 响应给用户
+        return new RestErrorResponse(stringBuffer.toString());
+    }
+
 }
