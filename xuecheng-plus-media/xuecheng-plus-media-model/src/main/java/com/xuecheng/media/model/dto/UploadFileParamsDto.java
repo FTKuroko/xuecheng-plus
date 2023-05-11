46fa51e3566898e0d737cc7d17efd3782a9e64de
Kuroko <982239116@qq.com>
2023-05-11 16:28:27 +0800

    上传图片

---
+++ xuecheng-plus-media/xuecheng-plus-media-model/src/main/java/com/xuecheng/media/model/dto/UploadFileParamsDto.java
@@ -0,0 +1,48 @@
+package com.xuecheng.media.model.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author Kuroko
+ * @description 上传普通文件请求参数
+ * @date 2023/5/11 14:54
+ */
+@Data
+@ToString
+public class UploadFileParamsDto {
+
+    /**
+     * 文件名称
+     */
+    private String filename;
+
+    /**
+     * 文件content-type
+     */
+    private String contentType;
+
+    /**
+     * 文件类型（文档，图片，视频）
+     */
+    private String fileType;
+    /**
+     * 文件大小
+     */
+    private Long fileSize;
+
+    /**
+     * 标签
+     */
+    private String tags;
+
+    /**
+     * 上传人
+     */
+    private String username;
+
+    /**
+     * 备注
+     */
+    private String remark;
+}
---
+++ xuecheng-plus-media/xuecheng-plus-media-model/src/main/java/com/xuecheng/media/model/dto/UploadFileResultDto.java
@@ -0,0 +1,13 @@
+package com.xuecheng.media.model.dto;
+
+import com.xuecheng.media.model.po.MediaFiles;
+import lombok.Data;
+
+/**
+ * @author Kuroko
+ * @description 封装上传普通文件成功响应结果
+ * @date 2023/5/11 14:34
+ */
+@Data
+public class UploadFileResultDto extends MediaFiles {
+}
