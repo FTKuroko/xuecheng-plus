1efa7587795163983d5ccfdd759b8c64610e9b65
Kuroko <982239116@qq.com>
2023-05-23 18:11:33 +0800

    课程发布，使用 ElasticSearch 保存课程索引信息

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/jobhandler/CoursePublishTask.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/jobhandler/CoursePublishTask.java
@@ -1,5 +1,11 @@
 package com.xuecheng.content.service.jobhandler;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import com.xuecheng.base.exception.XueChengPlusException;
+import com.xuecheng.content.feignclient.SearchServiceClient;
+import com.xuecheng.content.mapper.CoursePublishMapper;
+import com.xuecheng.content.model.po.CourseIndex;
+import com.xuecheng.content.model.po.CoursePublish;
 import com.xuecheng.content.service.CoursePublishService;
 import com.xuecheng.messagesdk.model.po.MqMessage;
 import com.xuecheng.messagesdk.service.MessageProcessAbstract;
@@ -7,6 +13,7 @@ import com.xuecheng.messagesdk.service.MqMessageService;
 import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -23,6 +30,10 @@ import java.util.concurrent.TimeUnit;
 public class CoursePublishTask extends MessageProcessAbstract {
     @Autowired
     CoursePublishService coursePublishService;
+    @Autowired
+    CoursePublishMapper coursePublishMapper;
+    @Autowired
+    SearchServiceClient searchServiceClient;
     // 任务调度入口
     @XxlJob("CoursePublishJobHandler")
     public void coursePublishJobHandler(){
@@ -97,6 +108,35 @@ public class CoursePublishTask extends MessageProcessAbstract {
      */
     private void saveCourseIndex(MqMessage mqMessage, long courseId){
         log.debug("保存课程索引信息，课程 id:{}", courseId);
+        // 消息 id
+        Long id = mqMessage.getId();
+        // 消息处理的 service
+        MqMessageService mqMessageService = this.getMqMessageService();
+        // 消息幂等性处理
+        int stageTwo = mqMessageService.getStageTwo(id);
+        if(stageTwo > 0){
+            log.debug("课程索引已处理，直接返回，课程 id:{}", courseId);
+            return;
+        }
 
+        Boolean result = saveCourseIndex(courseId);
+        if(result){
+            // 保存第一阶段
+            mqMessageService.completedStageTwo(id);
+        }
+    }
+
+    private Boolean saveCourseIndex(Long courseId){
+        // 取出课程发布信息
+        CoursePublish coursePublish = coursePublishMapper.selectById(courseId);
+        // 拷贝至课程索引对象
+        CourseIndex courseIndex = new CourseIndex();
+        BeanUtils.copyProperties(coursePublish, courseIndex);
+        // 远程调用搜索服务 api 添加课程信息到索引
+        Boolean add = searchServiceClient.add(courseIndex);
+        if(!add){
+            XueChengPlusException.cast("添加索引失败");
+        }
+        return true;
     }
 }
