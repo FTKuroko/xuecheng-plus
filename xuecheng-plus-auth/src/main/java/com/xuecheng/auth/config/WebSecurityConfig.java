0a0a6d1e4b5b415d48947a502142787a092f612a
Kuroko <982239116@qq.com>
2023-05-25 18:24:50 +0800

    JWT令牌登录验证

---
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/auth/config/WebSecurityConfig.java
@@ -0,0 +1,68 @@
+package com.xuecheng.auth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 安全管理配置
+ * @date 2022/9/26 20:53
+ */
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 配置认证管理 bean
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+
+    //配置用户信息服务
+    @Bean
+    public UserDetailsService userDetailsService() {
+        //这里配置用户信息,这里暂时使用这种方式将用户存储在内存中
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("Kuroko").password("123").authorities("p1").build());
+        manager.createUser(User.withUsername("xiaoming").password("456").authorities("p2").build());
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        //密码为明文方式
+        return NoOpPasswordEncoder.getInstance();
+//        return new BCryptPasswordEncoder();
+    }
+
+    //配置安全拦截机制
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()    // 配置请求授权规则
+                .antMatchers("/r/**")   // 指定需要进行访问控制的 URL 路径模式，访问/r开始的请求需要认证通过
+                .authenticated()    // 指定需要进行身份验证的请求
+                .anyRequest()   // 配置除了 antMatchers("**") 以外的所有请求都不需要身份验证
+                .permitAll()    // 其它请求全部放行
+                .and()
+                .formLogin()    // 配置登录页表单认证
+                .successForwardUrl("/login-success");   // 登录成功跳转到/login-success
+        http.logout().logoutUrl("/logout"); // 配置退出登录，指定退出登录的 URL
+    }
+
+
+
+}
